7f622522f9449b11a74034b6ae0d3decffc047ec
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-07-09 15:55:53 +0900

    Delete Programmers/코딩테스트입문/Day6 directory

--- Programmers/코딩테스트입문/Day6/2.py
+++
@@ -1,6 +0,0 @@
-def solution(n, control):
-    n += control.count('w') - control.count('s') + 10 * control.count('d') - 10 * control.count('a')
-    return n
-
-if __name__ =='__main__':
-    print(solution(0,"wsdawsdassw"))
--- Programmers/코딩테스트입문/Day6/4.py
+++
@@ -1,7 +0,0 @@
-def solution(arr, queries):
-    for i in range(len(queries)):
-        arr[queries[i][0]], arr[queries[i][1]] = arr[queries[i][1]], arr[queries[i][0]]
-    return arr
-
-if __name__ =='__main__':
-    print([0, 1, 2, 3, 4],[[0, 3],[1, 2],[1, 4]])
