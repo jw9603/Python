5db13d2f5d4099237618bc8840fd58a6b6f774bc
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-08-05 16:01:41 +0900

    14일차 - 8월 4일

---
+++ 99클럽/Beginner/14일차/1.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def isSubtreeSymmetric(left,right):
+            if not left and not right: # 왼쪽, 오른쪽이 없으면
+                return True
+            elif not left or not right: # 둘 중 하나만 있으면
+                return False
+            else: # 둘 다 없으면
+                return left.val == right.val and isSubtreeSymmetric(left.left, right.right) and isSubtreeSymmetric(left.right, right.left)
+        
+        return isSubtreeSymmetric(root,root)
