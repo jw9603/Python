7cad62f494032238a883c6d9859a59102923d894
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-10-28 22:21:39 +0900

    수 정렬하기 2 - 실버 5단계

---
+++ BaekJoon/단계별로_풀어보기/정렬_13단계/2751.py
@@ -0,0 +1,19 @@
+# 2751 - 수 정렬하기 2
+# https://www.acmicpc.net/problem/2751
+
+import sys
+
+def sol(n_list):
+    
+    n_list.sort()
+    
+    for i in n_list:
+        print(i)
+        
+N = int(sys.stdin.readline().strip())
+n_list = []
+for _ in range(N):
+    n_list.append(int(sys.stdin.readline().strip()))
+
+sol(n_list)
+    
