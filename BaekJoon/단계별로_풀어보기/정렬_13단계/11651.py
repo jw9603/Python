bd12d1488b0e36d7277048f5e007e0d6de1bdae5
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-10-30 22:22:25 +0900

    좌표 정렬하기 2 - 실버 5단계

---
+++ BaekJoon/단계별로_풀어보기/정렬_13단계/11651.py
@@ -0,0 +1,20 @@
+# 11651 - 좌표 정렬하기 2
+# https://www.acmicpc.net/problem/11651
+
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+dot_list = []
+
+for _ in range(N):
+        x, y = map(int,sys.stdin.readline().split())
+        dot_list.append([x,y])
+        
+def sol(dot_list):
+    dot_list.sort(key=lambda x: (x[1],x[0]))
+
+    for x,y in dot_list:
+        print(x,y)
+
+sol(dot_list=dot_list)
