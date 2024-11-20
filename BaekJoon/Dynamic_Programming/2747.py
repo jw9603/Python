a8ac5e9e551d8124392bb76916f1c135e6d283c4
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-11-20 21:16:36 +0900

    2747. 피보나치 수 - 브론즈 2단계

---
+++ BaekJoon/Dynamic_Programming/2747.py
@@ -0,0 +1,34 @@
+# 2747. 피보나치 수
+# https://www.acmicpc.net/problem/2747
+
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+# Top-Down
+memo = {}
+def fibo_topdown(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n in memo:
+        return memo[n]
+    
+    memo[n] = fibo_topdown(n - 1) + fibo_topdown(n - 2)
+    return memo[n]
+# Botton-Up
+def fibo_bottomup(n):
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    
+    for i in range(2,n+1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
+
+print(fibo_topdown(n))
+print(fibo_bottomup(n))
+        
+
+
+    
