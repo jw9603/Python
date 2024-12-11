5c35971a73527262db48943c15600585ea8e0324
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-11 21:54:24 +0900

    4949. 균형잡힌 세상 - 실버 4단계

---
+++ 유형별풀이/Stack_Queue/Baekjoon/4949.py
@@ -0,0 +1,29 @@
+# 4949. 균형잡힌 세상
+# https://www.acmicpc.net/problem/4949
+import sys
+
+while True:
+    text = sys.stdin.readline().rstrip()
+    if text == '.':
+        break
+    stack = []
+    flag = True
+    
+    for t in text:
+        if t == '(' or t == '[':
+            stack.append(t)
+        elif t == ')':
+            if not stack or stack[-1] == '[':
+                flag = False
+                break
+            stack.pop()
+        elif t == ']':
+            if not stack or stack[-1] =='(':
+                flag = False
+                break
+            stack.pop()
+        
+    if not stack and flag:
+        print('yes')
+    else:
+        print('no')                
