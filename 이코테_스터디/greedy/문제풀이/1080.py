9d6c17c0ce975c490cec6fbaf46ba73c5d1fda54
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-18 12:25:39 +0900

    백준 1080. 행렬 - 실버 1단계

---
+++ 이코테_스터디/greedy/문제풀이/1080.py
@@ -0,0 +1,26 @@
+# 1080. 행렬
+# https://www.acmicpc.net/problem/1080
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+A = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
+B = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
+cnt = 0
+flag = True
+
+def convert_matrix(i, j, arr):
+    for x in range(i, i + 3):
+        for y in range(j, j + 3):
+            arr[x][y] = 1 - arr[x][y]
+
+for i in range(N - 2):
+    for j in range(M - 2):
+        if A[i][j] != B[i][j]:
+            convert_matrix(i, j, A)
+            cnt += 1
+for i in range(N):
+    for j in range(M):
+        if A[i][j] != B[i][j]:
+            flag = False
+print(-1 if not flag else cnt)
