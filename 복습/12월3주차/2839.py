46c3f323196d03f4f9c7d0fb145ccba108ef42bc
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-21 12:46:47 +0900

    백준 2839. 설탕 배달 - 12월 21일 복습

---
+++ 복습/12월3주차/2839.py
@@ -0,0 +1,15 @@
+# 백준 2839. 설탕 배달
+# https://www.acmicpc.net/problem/2839
+import sys
+N = int(sys.stdin.readline().strip())
+def sol(N):
+    cnt = 0
+    while N >=0:
+        if N % 5 == 0:
+            cnt += N // 5
+            return cnt
+        else:
+            N -=3
+            cnt += 1
+    return -1
+print(sol(N=N))
