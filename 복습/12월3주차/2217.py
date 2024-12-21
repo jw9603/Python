0af6b79ba81dcc44fa69853e441efe4d85af0bba
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-21 13:04:40 +0900

    백준 2217. 로프 - 12월 21일

---
+++ 복습/12월3주차/2217.py
@@ -0,0 +1,11 @@
+# 백준 2217. 로프
+# https://www.acmicpc.net/problem/2217
+import sys
+N = int(sys.stdin.readline().strip())
+rope = sorted([int(sys.stdin.readline().strip()) for _ in range(N)])
+result = []
+for r in rope:
+    result.append(r * N)
+    N -= 1
+print(max(result))
+    
