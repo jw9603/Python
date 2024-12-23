fce9298ccd6c24e6014084aec4ecec133148ff66
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-23 15:09:07 +0900

    Update 1789.py

--- BaekJoon/알고리즘유형/그리디알고리즘/1789.py
+++ BaekJoon/알고리즘유형/그리디알고리즘/1789.py
@@ -10,3 +10,12 @@ def sol(S):
         total += N
     return N
 print(sol(S=S))
+############ 최적화 ####################
+import sys
+import math
+S = int(sys.stdin.readline().strip())
+def sol(S):
+    N = int((-1 + math.sqrt(1 + 8 * S)) // 2)
+    return N
+print(sol(S=S))
+############ 최적화 ####################
