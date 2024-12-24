a47bf52e304c66fcd6ceb18b778a4d79d0050152
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-24 12:56:21 +0900

    백준 10610. 30 - 실버 4단계

---
+++ BaekJoon/알고리즘유형/그리디알고리즘/10610.py
@@ -0,0 +1,12 @@
+# 백준 10610. 30
+# https://www.acmicpc.net/problem/10610
+import sys
+N = int(sys.stdin.readline().strip())
+def sol(N):
+    if '0' not in N or len(N) == 1:
+        return -1
+    if sum(map(int, N)) % 3 != 0:
+        return -1
+    return ''.join(sorted(N, reverse=True))
+print(sol(N=N))
+        
