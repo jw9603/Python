96bba07cc7347db311b2953fca8597282e157839
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-26 17:12:00 +0900

    백준 1138. 한 줄로 서기 - 실버 2단계

---
+++ BaekJoon/알고리즘유형/그리디알고리즘/1138.py
@@ -0,0 +1,23 @@
+# 백준 1138. 한 줄로 서기
+# https://www.acmicpc.net/problem/1138
+import sys
+N = int(sys.stdin.readline().strip())
+conditions = list(map(int, sys.stdin.readline().split()))
+
+def sol(N, conditions):
+    result = [0] * N
+    
+    for height in range(1, N + 1): # if N = 4, then height: 1, 2, 3, 4
+        cnt = conditions[height - 1] # cnt = 2, 1, 1, 0
+        empty_cnt = 0
+        for i in range(N):
+            if result[i] == 0:
+                if empty_cnt == cnt:
+                    result[i] = height
+                empty_cnt += 1
+    return result
+print(*sol(N=N, conditions=conditions))
+                
+
+                
+    
