5c2a0517144f0bd1111ba02edf61b1a7da14ce9d
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-26 15:07:11 +0900

    백준 4796. 캠핑 - 브론즈 1단계

---
+++ BaekJoon/알고리즘유형/그리디알고리즘/4796.py
@@ -0,0 +1,14 @@
+# 백준 4796. 캠핑
+# https://www.acmicpc.net/problem/4796
+import sys
+i = 1
+while True:
+    L, P, V = map(int, sys.stdin.readline().split())
+    if L ==0 and P ==0 and V == 0:
+        break
+    
+    result = (V // P) * L + min(V % P, L)
+    print(f"Case {i}: {result}")
+    i += 1
+    
+        
