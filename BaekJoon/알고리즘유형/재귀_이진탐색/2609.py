eca7f15fd95250fdfa0eaa9fe045947da2288389
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-01-07 11:48:04 +0900

    백준 2609. 최대공약수와 최소공배수 - 브론즈 1단계

---
+++ BaekJoon/알고리즘유형/재귀_이진탐색/2609.py
@@ -0,0 +1,11 @@
+# 백준 2609. 최대 공약수와 최소공배수
+# https://www.acmicpc.net/problem/2609
+import sys
+a, b = map(int, sys.stdin.readline().split())
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+def lcm(a,b):
+    return a * b // gcd(a, b)
+print(gcd(a, b), lcm(a,b), sep='\n')
