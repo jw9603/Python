2ad76593e29a1a4850c84ad308e4658e6b93e50c
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-01-09 11:21:47 +0900

    백준 2606. 바이러스 - 실버 3단계

--- 이코테_스터디/dfs_bfs/문제 풀이/2606.py
+++ 이코테_스터디/dfs_bfs/문제 풀이/2606.py
@@ -1,63 +1,50 @@
 # 백준 2606. 바이러스
 # https://www.acmicpc.net/problem/2606
 
-
-########################## BFS 풀이 #############################
+####################### BFS 풀이 ###########################
+from collections import deque, defaultdict
 import sys
-from collections import deque
-from collections import defaultdict
+
 n = int(sys.stdin.readline().strip())
 m = int(sys.stdin.readline().strip())
+graph = defaultdict(list)
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-def sol(n, m):
-    graph = defaultdict(list)
-    
-    for _ in range(m):
-        a, b = map(int, sys.stdin.readline().split())
-        graph[a].append(b)
-        graph[b].append(a)
-        
-    visited = [False] * (n + 1)
+def sol(graph, start_v):
+    visited = [start_v]
     queue = deque()
-    queue.append(1)
-    visited[1] = True
-    cnt = 0
+    queue.append(start_v)
     
     while queue:
         cur_v = queue.popleft()
         for next_v in graph[cur_v]:
-            if not visited[next_v]:
-                visited[next_v] = True
+            if next_v not in visited:
+                visited.append(next_v)
                 queue.append(next_v)
-                cnt += 1
-    return cnt
-print(sol(n=n, m=m))
-########################## BFS 풀이 #############################
+    return len(visited) - 1
+print(sol(graph=graph, start_v=1))
+####################### BFS 풀이 ###########################
 
-########################## DFS 풀이 #############################
-import sys
+####################### DFS 풀이 ###########################
 from collections import defaultdict
+import sys
 n = int(sys.stdin.readline().strip())
 m = int(sys.stdin.readline().strip())
-
-def sol(n, m):
-    graph = defaultdict(list)
+graph = defaultdict(list)
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
     
-    for _ in range(m):
-        a, b = map(int, sys.stdin.readline().split())
-        graph[a].append(b)
-        graph[b].append(a)
-    visited = [False] * (n + 1)
-    cnt = 0
-    def dfs(node):
-        nonlocal cnt
-        visited[node] = True
-        for next_node in graph[node]:
-            if not visited[next_node]:
-                cnt += 1
-                dfs(next_node)
-        
-    dfs(1)
-    print(cnt)
-sol(n=n, m=m)
-########################## DFS 풀이 #############################
+def sol(graph, start_v, visited):
+    visited.append(start_v)
+    
+    for next_v in graph[start_v]:
+        if next_v not in visited:
+            sol(graph, next_v, visited)
+    return len(visited) - 1
+print(sol(graph=graph, start_v=1, visited=[]))
+####################### DFS 풀이 ###########################
