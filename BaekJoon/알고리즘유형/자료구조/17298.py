880f20b072dad7813445b7f036e6db97e02d4ec7
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-01-31 11:55:32 +0900

    백준 17298. 오큰수 - 골드 4단계

---
+++ BaekJoon/알고리즘유형/자료구조/17298.py
@@ -0,0 +1,16 @@
+# 백준 17298. 오큰수
+# https://www.acmicpc.net/problem/17298
+import sys
+N = int(sys.stdin.readline().strip())
+A = list(map(int, sys.stdin.readline().split()))
+
+def sol(N, A):
+    NGE = [-1] * N
+    stack = [0]
+    
+    for i in range(1, N):
+        while stack and A[stack[-1]] < A[i]:
+            NGE[stack.pop()] = A[i]
+        stack.append(i)
+    return NGE
+print(*sol(N=N, A=A))
