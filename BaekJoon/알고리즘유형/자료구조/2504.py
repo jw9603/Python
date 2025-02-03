3e95b949d32df613d5a5877af29cb98fec46b8b3
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-02-03 21:06:29 +0900

    백준 2504. 괄호의 값 - 골드 5단계

---
+++ BaekJoon/알고리즘유형/자료구조/2504.py
@@ -0,0 +1,33 @@
+# 백준 2504. 괄호의 값
+# https://www.acmicpc.net/problem/2504
+import sys
+s = sys.stdin.readline().strip()
+def sol(s):
+    total = 0
+    tmp = 1
+    stack = []
+    for i, char in enumerate(s):
+        if char == '(':
+            stack.append(char)
+            tmp *= 2
+        elif char == '[':
+            stack.append(char)
+            tmp *= 3
+        elif char == ')':
+            if not stack or stack[-1] != '(':
+                return 0
+            if s[i - 1] == '(':
+                total += tmp
+            
+            stack.pop()
+            tmp //= 2
+        elif char == ']':
+            if not stack or stack[-1] != '[':
+                return 0
+            if s[i - 1] == '[':
+                total += tmp
+            
+            stack.pop()
+            tmp //= 3
+    return total if not stack else 0
+print(sol(s=s))
