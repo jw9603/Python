8c95810143580ab86d528befe63c48fede5df60e
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-02-06 23:46:27 +0900

    백준 19532. 수학은 비대면강의입니다 - 브론즈 2단계

---
+++ BaekJoon/알고리즘유형/완전탐색/19532.py
@@ -0,0 +1,11 @@
+# 백준 19532. 수학은 비대면강의입니다
+# https://www.acmicpc.net/problem/19532
+import sys
+a, b, c, d, e, f = map(int, sys.stdin.readline().split())
+
+def sol(a, b, c, d, e, f):
+    for i in range(-999, 1000):
+        for j in range(-999, 1000):
+            if a * i + b * j == c and d * i + e * j == f:
+                return i, j
+print(*sol(a=a, b=b, c=c, d=d, e=e, f=f))
