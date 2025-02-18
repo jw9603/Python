9a7bec2b79a3aadf25596d80571c570954813656
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-02-18 16:21:27 +0900

    백준 1182. 부분수열의 합 - 실버 2단계

--- BaekJoon/알고리즘유형/완전탐색/1182.py
+++ BaekJoon/알고리즘유형/완전탐색/1182.py
@@ -1,22 +1,28 @@
 # 백준 1182. 부분수열의 합
 # https://www.acmicpc.net/problem/1182
 import sys
-N, S = map(int, sys.stdin.readline().split())
-nums = list(map(int, sys.stdin.readline().split()))
 
-cnt = 0
-def sol(search_idx, current_sum):
-    global cnt
+def main():
+    N, S = map(int, sys.stdin.readline().split())
+    num = list(map(int, sys.stdin.readline().split()))
     
-    if search_idx == N:
-        if current_sum == S:
-            cnt += 1
-        return
+    cnt = 0
     
-    sol(search_idx + 1, current_sum)
-    sol(search_idx + 1, current_sum + nums[search_idx])
-
-sol(0, 0)
-if S == 0:
-    cnt -= 1
-print(cnt)
+    def sol(search_idx, current_sum):
+        nonlocal cnt
+        # 1. base case
+        if search_idx == N:
+            if current_sum == S:
+                cnt += 1
+            return
+        
+        sol(search_idx + 1, current_sum)
+        sol(search_idx + 1, current_sum + num[search_idx])
+    sol(0, 0)
+    
+    if S == 0:
+        cnt -= 1
+    print(cnt)
+    
+if __name__ == '__main__':
+    main()
