2c94f79c5f3463029ca77c1547d012d396605ebe
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-03-07 22:46:09 +0900

    Update leetcode46.py

--- killingcamp/1주차(완전탐색,이진탐색)/leetcode46.py
+++ killingcamp/1주차(완전탐색,이진탐색)/leetcode46.py
@@ -1,7 +1,7 @@
 # Leetcode 46. Permutations
 # https://leetcode.com/problems/permutations/
 # https://leetcode.com/problems/word-search/submissions/1563717945
-# 시간 복잡도: O(N!)
+# 1. 재귀 시간 복잡도: O(N!)
 class Solution(object):
     def permute(self, nums):
         """
@@ -22,7 +22,40 @@ class Solution(object):
                     curr.pop()
         dfs([])
         return ans
-        
+# 2. 재귀 (in 연산자 사용 안하기)
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = []
+        used = [False] * (len(nums))
+        def dfs(curr):
+            # 1. base case
+            if len(curr) == len(nums):
+                ans.append(curr[:])
+                return
+            
+            for i in range(len(nums)):
+                if not used[i]:
+                    used[i] = True
+                    curr.append(nums[i])
+                    dfs(curr)
+                    curr.pop()
+                    used[i] = False
+        dfs([])
+        return ans
+    
+# 3. itertools
+from itertools import permutations
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(map(list, permutations(nums)))
         
 if __name__ == '__main__':
     nums1, nums2, nums3 = [1, 2, 3], [0, 1], [1]
