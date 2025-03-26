fd4b20d1b27cbf1b2f2e391989926633ec2f9a4e
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-03-26 11:53:40 +0900

    백준 1182. 부분수열의 합 - 실버 2단계

--- BaekJoon/알고리즘유형/완전탐색/1182.py
+++ BaekJoon/알고리즘유형/완전탐색/1182.py
@@ -1,28 +1,26 @@
 # 백준 1182. 부분수열의 합
 # https://www.acmicpc.net/problem/1182
 import sys
+def sol(idx, current_sum):
+    global N, S, nums, cnt
+    # 1. base case
+    if idx == N:
+        if current_sum == S:
+            cnt += 1
+        return
+    
+    sol(idx + 1, current_sum)
+    sol(idx + 1, current_sum + nums[idx])
 
 def main():
+    global N, S, nums, cnt
     N, S = map(int, sys.stdin.readline().split())
-    num = list(map(int, sys.stdin.readline().split()))
-    
+    nums = list(map(int, sys.stdin.readline().split()))
+
     cnt = 0
-    
-    def sol(search_idx, current_sum):
-        nonlocal cnt
-        # 1. base case
-        if search_idx == N:
-            if current_sum == S:
-                cnt += 1
-            return
-        
-        sol(search_idx + 1, current_sum)
-        sol(search_idx + 1, current_sum + num[search_idx])
     sol(0, 0)
-    
-    if S == 0:
-        cnt -= 1
-    print(cnt)
-    
+
+    print(cnt - 1 if not S else cnt)
+
 if __name__ == '__main__':
     main()
