ac2c6454388d87a86b199c78681b3da6b360b4f0
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-03-28 21:55:20 +0900

    Update 14499.py

--- 삼성간다/step2/14499.py
+++ 삼성간다/step2/14499.py
@@ -1,50 +1,57 @@
 # 백준 14499. 주사위 굴리기
 # https://www.acmicpc.net/problem/14499
-import sys
-def roll_dice(dice, direction):
-   top, bottom, north, south, west, east = dice
-
-   if direction == 1: # 동
-      return [west, east, north, south, bottom, top]
-   elif direction == 2: # 서
-       return [east, west, north, south, top, bottom]
-   elif direction == 3: # 북
-       return [south, north, top, bottom, west, east]
-   elif direction == 4:
-       return [north, south, bottom, top, west, east]
-
-def main():
-
-    N, M, x, y, K = map(int, sys.stdin.readline().split())
-    board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-    commands = list(map(int, sys.stdin.readline().split()))
+# 백준 14499. 주사위 굴리기
+# https://www.acmicpc.net/problem/14499
+def print_dice_result(commands, dice, x, y):
+    global n, m, board
 
     # 동, 서, 북, 남
-    dx = [0, 0, -1, 1] 
+    dx = [0, 0, -1, 1]
     dy = [1, -1, 0, 0]
 
-    dice = [0] * 6
-
     for command in commands:
-        direction = command - 1
-        next_x, next_y = x + dx[direction], y + dy[direction]
+        d = command - 1
 
-        if next_x < 0 or next_x >= N or next_y < 0 or next_y >= M:
+        next_x, next_y = x + dx[d], y + dy[d]
+
+        if next_x < 0 or next_x >= n or next_y < 0 or next_y >= m:
             continue
         
-        x, y = next_x, next_y
-
         dice = roll_dice(dice, command)
 
-        # 이동한 칸에 쓰여있는 수가 0이면, 주사위 바닥면에 쓰여있는 수가 칸에 복사된다.
+        x, y = next_x, next_y
+
         if board[x][y] == 0:
             board[x][y] = dice[1]
         else:
-        # 0이 아니면, 칸에 쓰여있는 수가 주사위의 바닥면에 복사되고 칸에 쓰여있는 수가 0이 된다.
             dice[1] = board[x][y]
             board[x][y] = 0
         
         print(dice[0])
+
+
+def roll_dice(dice, command):
+    top, bottom, north, south, west, east = dice
+
+    if command == 1:   # 동
+        return [west, east, north, south, bottom, top]
+    elif command == 2: # 서
+        return [east, west, north, south, top, bottom]
+    elif command == 3: # 북
+        return [south, north, top, bottom, west, east]
+    else:              # 남
+        return [north, south, bottom, top, west, east]
+        
+
+def main():
+    global n, m, board
+    n, m, x, y, k = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    commands = list(map(int, input().split()))
+
+    dice = [0] * 6
+
+    print_dice_result(commands, dice, x, y)
 
 if __name__ == '__main__':
     main()
