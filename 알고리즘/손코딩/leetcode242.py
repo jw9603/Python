01c5bac8dea3d8c9e6bc14059b3f6b200311403b
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-07-03 11:58:23 +0900

    LeetCode 242. Valid Anagram - Easy

---
+++ 알고리즘/손코딩/leetcode242.py
@@ -0,0 +1,44 @@
+# LeetCode 242. Valid Anagram
+# https://leetcode.com/problems/valid-anagram/description/
+class Solution(object):
+    # 1. 정렬
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
+    
+    # 2. Hashing
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_dict = {}
+        t_dict = {}
+        
+        for char in s:
+            if char in s_dict:
+                s_dict[char] += 1
+            else:
+                s_dict[char] = 1
+        
+        for char in t:
+            if char in t_dict:
+                t_dict[char] += 1
+            else:
+                t_dict[char] = 1
+        return s_dict == t_dict  
+    
+    # 3. Counter사용
+    from collections import Counter
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return Counter(s) == Counter(t)
