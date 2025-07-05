28a5fb5a25cce792e980cfd76ad254c29ec5dc94
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-07-05 14:54:37 +0900

    LeetCode 217. Contains Duplicate - Easy

---
+++ 알고리즘/손코딩/leetcode217.py
@@ -0,0 +1,9 @@
+# LeetCode 217. Contains Duplicate
+# https://leetcode.com/problems/contains-duplicate/
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))   
