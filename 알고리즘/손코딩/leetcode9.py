e04b75017f60ee713ba49139f6850983c14ae443
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-07-05 16:06:13 +0900

    Delete 알고리즘/손코딩 directory

--- 알고리즘/손코딩/leetcode9.py
+++
@@ -1,9 +0,0 @@
-# LeetCode 9. Palindrome Number
-# https://leetcode.com/problems/palindrome-number/description/
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        return str(x) == str(x)[::-1]
