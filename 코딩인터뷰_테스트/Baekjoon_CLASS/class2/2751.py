56c6d7eef064be6bf9b7c8f739b6f99aa676a556
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-07-17 15:47:58 +0900

    Delete 코딩인터뷰_테스트/Baekjoon_CLASS/class2/2751.py

--- 코딩인터뷰_테스트/Baekjoon_CLASS/class2/2751.py
+++
@@ -1,9 +0,0 @@
-# 백준 2751. 수 정렬하기 2
-# https://www.acmicpc.net/problem/2751
-def main():
-    N = int(input().strip())
-    nums = list(int(input().strip()) for _ in range(N))
-    print(*sorted(nums), sep='\n')
-
-if __name__ == '__main__':
-    main()
